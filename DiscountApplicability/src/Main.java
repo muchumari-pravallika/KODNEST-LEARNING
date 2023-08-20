7c4c1e86a3efb320271ec482c6fdd764376622a7
muchumari-pravallika <135297889+muchumari-pravallika@users.noreply.github.com>
2023-08-20 13:21:16 +0530

    Practice Programs
    Practice Programs on Concepts of Java

---
+++ DiscountApplicability/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void checkDiscount(double purchaseAmount)
+	{
+		if(purchaseAmount>100)
+		{
+			System.out.println("Discount Acceptable");
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		double purchaseAmount=scan.nextDouble();
+		checkDiscount(purchaseAmount);
+		scan.close();
+	}
+
+
+}
---
+++ DiscountApplicability/src/com/kodnest/discountapplicability/Main.java
@@ -0,0 +1,22 @@
+package com.kodnest.discountapplicability;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void checkDiscount(double purchaseAmount)
+	{
+		if(purchaseAmount>100)
+		{
+			System.out.println("Discount Acceptable");
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		double purchaseAmount=scan.nextDouble();
+		checkDiscount(purchaseAmount);
+		scan.close();
+	}
+
+}
